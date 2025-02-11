4eae6e035b4f380ceab8fade8a5ba17b50919856
HARSH PRATEEK <piscesscorpio11819@gmail.com>
2025-02-12 02:41:25 +0530

    Fixed session storage

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(session({
     resave:false,
     saveUninitialized:false,
     cookie:{
-        secure:process.env.NODE_ENV === "production",
+        secure:false,
         maxAge:30000000,
         sameSite:"none",
         httpOnly:true},
